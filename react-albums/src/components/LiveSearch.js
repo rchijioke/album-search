84f0552dc9461d995cce8f47c996e57d412668ac
Roy <ekeneroy@gmail.com>
2023-07-23 16:38:27 -0500

    Axios and React

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,11 +1,26 @@
-import React, { Fragment, useState } from "react";
-
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
 
 export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const baseURL = "https://itunes.apple.com/search";
+    const params = {
+      term: term,
+      country: "CA",
+      media: "music",
+      entity: "album",
+      attribute: "artistTerm"
+    };
+    const searchURL = `${baseURL}?term=${params.term}&country=${params.country}&media=${params.media}&entity=${params.entity}&attribute=${params.attribute}`;
+    axios.get(searchURL).then(response => {
+      setResults(response.data.results);
+    });
+  },[term]);
 
   return (
     <Fragment>
